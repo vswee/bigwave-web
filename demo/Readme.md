7dc93f9aa523b33d2c8322f27bcde52deed57d35
Jetbrains <jetbrains10@protonmail.com>
2021-06-12 15:32:52 +0500

    added server api

---
+++ demo/Readme.md
@@ -0,0 +1,5 @@
+## How to run
+
+1. npm install -g http-server
+2. cd demo
+3. http-server -S -C cert.pem -o 
---
+++ server/server.js
@@ -0,0 +1,24 @@
+var express = require('express')
+const fs = require('fs')
+const path = require('path')
+var cors = require('cors')
+
+var app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.post('/signup', function (req, res) {
+  const { email } = req.body
+  console.log(email)
+  
+  fs.appendFile(path.join(__dirname, '/data/signup-data.txt'), `${email}\n`, (error) => {
+    if (error) {
+      res.status(400).send({ message: 'Some error occured', error })
+      throw err
+    };
+    res.status(200).send({ message: 'Email added successfully' })
+  });
+})
+
+app.listen(3000, () => console.log('Server started on 3000'))
